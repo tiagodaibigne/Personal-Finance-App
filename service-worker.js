31a2213d0e938ce364e1b0e82bce9ac6e814dcf5
tiagodaibigne <66746663+tiagodaibigne@users.noreply.github.com>
2026-06-30 09:34:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // and cache-first for fonts (which rarely change).
 
 // Bump this version string on each deploy to retire old caches cleanly.
-var CACHE = 'pba-cache-v81';
+var CACHE = 'pba-cache-v82';
 
 // The core file(s) that make up the app shell.
 var APP_SHELL = [
